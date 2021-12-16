fce09a5649a81a86f2cfd2847336a602fa4100a6
Vlad Crauciuc <vlad.crauciuc1@gmail.com>
2021-12-16 18:52:41 +0200

    change-color

--- my-project/pages/index.jsx
+++ my-project/pages/index.jsx
@@ -6,17 +6,17 @@ export default function Home() {
       <ul className='flex'>
         <li className='mr-8'>
           <Link href='about'>
-            <a className='py-2 px-8 border-b-2 border-sky-800 hover:bg-sky-800 text-white block'>About</a>
+            <a className='py-2 px-8 border-b-2 border-orange-400 hover:bg-orange-400 text-white block'>About</a>
           </Link>
         </li>
         <li className='mr-8'>
           <Link href='about'>
-            <a className='py-2 px-8 border-b-2 border-sky-800 hover:bg-sky-800 text-white block'>Portfolio</a>
+            <a className='py-2 px-8 border-b-2 border-orange-400 hover:bg-orange-400 text-white block'>Portfolio</a>
           </Link>
         </li>
         <li>
           <Link href='about'>
-            <a className='py-2 px-8 border-b-2 border-sky-800 hover:bg-sky-800 text-white block'>Contact</a>
+            <a className='py-2 px-8 border-b-2 border-orange-400 hover:bg-orange-400 text-white block'>Contact</a>
           </Link>
         </li>
       </ul>
