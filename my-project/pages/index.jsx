b153a3327fabf6e44c6dea1b3531de98ce80dcb6
Vlad Crauciuc <vlad.crauciuc1@gmail.com>
2021-12-18 20:08:31 +0200

    about

--- my-project/pages/index.jsx
+++ my-project/pages/index.jsx
@@ -1,47 +1,165 @@
-import Link from 'next/link';
 import Script from 'next/script';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
-    <nav className='flex justify-center sm:justify-end bg-white/10 sm:bg-white/0 sticky top-0 w-full'>
-      <ul className='flex mt-4 sm:mr-8 border-b-2'>
-        <li className='mr-4'>
-          <Link href='/'>
-            <a className='menu-link' id='aboutLink'>About</a>
-          </Link>
-        </li>
-        <li className='mr-4'>
-          <Link href='/'>
-            <a className='menu-link' id='portfolioLink'>Portfolio</a>
-          </Link>
-        </li>
-        <li>
-          <Link href='/'>
-            <a className='menu-link' id='contactLink'>Contact</a>
-          </Link>
-        </li>
-      </ul>
-    </nav>
-    <div className="container mx-auto">
-    
+      <nav className="flex justify-center sm:justify-end bg-white/10 sm:bg-white/0 sticky top-0 w-full">
+        <ul className="flex mt-4 sm:mr-8 border-b-2">
+          <li className="mr-4">
+            <Link href="/">
+              <a className="menu-link" id="aboutLink">
+                About
+              </a>
+            </Link>
+          </li>
+          <li className="mr-4">
+            <Link href="/">
+              <a className="menu-link" id="portfolioLink">
+                Portfolio
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a className="menu-link" id="contactLink">
+                Contact
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
 
-      <section className="container border-2 border-red-500 h-screen flex justify-center items-center mt-32 mb-4 text-white" id='about'>
-        <span>This is the About section</span>
-      </section >
-      <section className="container border-2 border-blue-800 h-screen flex justify-center items-center mb-4 text-white" id='portfolio'>
-        <span>This is the Portfolio section</span>
-      </section >
-      <section className="container border-2 border-green-800 h-screen flex justify-center items-center text-white" id='contact'>
-        <span>This is the contact section</span>
-      </section >
+      <div className="container mx-auto">
+        <section
+          className="my-16 px-5 sm:px-0 flex flex-col justify-center items-center text-center *bg-sky-500 gap-4"
+          id="intro"
+        >
+          <span className="text-white text-7xl sm:text-8xl font-light *bg-orange-400">
+            Hello there.
+          </span>
+          <span className="text-white text-5xl sm:text-8xl font-light *bg-orange-400">
+            My name is <span className="font-semibold">Vlad</span>.
+          </span>
+          <span className="text-white text-5xl font-light mt-4 *bg-orange-400">
+            I'm a graphic designer and web developer.
+          </span>
+        </section>
 
-        <Link href='/'>
-          <a id='back2Top' className='py-2 px-4 bg-white/10 text-white fixed bottom-0 right-0'>Back to top</a>
+        <section
+          className="container border-2 border-red-500 h-screen flex justify-center items-center mb-4 text-white"
+          id="about"
+        >
+          <div className="w-16 h-16 ml-6 mr-6">
+            <Image
+              alt="HTML5"
+              src="/images/html5-logo.png"
+              width={1}
+              height={1}
+              layout="responsive"
+              objectFit="contain"
+            />
+          </div>
+          <div className="w-16 h-16 mr-6">
+            <Image
+              alt="CSS3"
+              src="/images/css3-logo.png"
+              width={1}
+              height={1}
+              layout="responsive"
+              objectFit="contain"
+            />
+          </div>
+          <div className="w-16 h-16 mr-6">
+            <Image
+              alt="Javascript"
+              src="/images/javascript-logo.png"
+              width={1}
+              height={1}
+              layout="responsive"
+              objectFit="contain"
+            />
+          </div>
+          <div className="w-16 h-16 mr-6">
+            <Image
+              alt="Javascript"
+              src="/images/nextjs-logo.png"
+              width={1}
+              height={1}
+              layout="responsive"
+              objectFit="contain"
+            />
+          </div>
+          <div className="w-16 h-16 mr-6">
+            <Image
+              alt="Javascript"
+              src="/images/photoshop-logo.png"
+              width={1}
+              height={1}
+              layout="responsive"
+              objectFit="contain"
+            />
+          </div>
+          <div className="w-16 h-16 mr-6">
+            <Image
+              alt="Javascript"
+              src="/images/illustrator-logo.png"
+              width={1}
+              height={1}
+              layout="responsive"
+              objectFit="contain"
+            />
+          </div>
+          <div className="w-16 h-16 mr-6">
+            <Image
+              alt="Javascript"
+              src="/images/sass-logo.png"
+              width={1}
+              height={1}
+              layout="responsive"
+              objectFit="contain"
+            />
+          </div>
+          <div className="w-16 h-16 mr-6">
+            <Image
+              alt="Javascript"
+              src="/images/github-logo.png"
+              width={1}
+              height={1}
+              layout="responsive"
+              objectFit="contain"
+            />
+          </div>
+        </section>
+        <section
+          className="container border-2 border-blue-800 h-screen flex justify-center items-center mb-4 text-white"
+          id="portfolio"
+        >
+          <span>This is the Portfolio section</span>
+        </section>
+        <section
+          className="container border-2 border-green-800 h-screen flex justify-center items-center text-white"
+          id="contact"
+        >
+          <span>This is the contact section</span>
+        </section>
+
+        <Link href="/">
+          <a
+            id="back2Top"
+            className="py-2 px-4 bg-white/10 text-white fixed bottom-0 right-0"
+          >
+            Back to top
+          </a>
         </Link>
 
-      <Script id='script' src='/js/script.js' strategy='afterInteractive'></Script>
-    </div>
+        <Script
+          id="script"
+          src="/js/script.js"
+          strategy="afterInteractive"
+        ></Script>
+      </div>
     </>
-  )
+  );
 }
