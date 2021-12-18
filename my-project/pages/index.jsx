c05e5a85c89caa6046d32e95316656bd2dfef15f
Vlad Crauciuc <vlad.crauciuc1@gmail.com>
2021-12-18 20:18:17 +0200

    about

--- my-project/pages/index.jsx
+++ my-project/pages/index.jsx
@@ -33,7 +33,7 @@ export default function Home() {
 
       <div className="container mx-auto">
         <section
-          className="my-6 sm:my-16 px-5 sm:px-0 flex flex-col justify-center items-center text-center *bg-sky-500 gap-4"
+          className="my-10 sm:my-16 px-5 sm:px-0 flex flex-col justify-center items-center text-center *bg-sky-500 gap-4"
           id="intro"
         >
           <span className="text-white text-6xl sm:text-8xl font-light *bg-orange-400">
